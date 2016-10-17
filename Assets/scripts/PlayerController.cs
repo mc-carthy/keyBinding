579d91780f58082cc26ccb4bb8fe5a77149676bf
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-17 10:11:10 +0100

    Add basic jump

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	private bool doJump = false;
+	[SerializeField]
+	private float jumpSpeed = 10f;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	private void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			doJump = true;
+		}
+	}
+
+	private void FixedUpdate () {
+		if (doJump) {
+			doJump = false;
+			rb.velocity = Vector2.up * jumpSpeed;
+		}
+	}
+}
